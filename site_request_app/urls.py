8059e380f5f67148c4d1b62888a4f52e0d614ee9
Adam LeBlanc <leblan7@gmail.com>
2015-05-28 19:18:06 -0700

    might have fixed css not working

--- site_request_app/urls.py
+++ site_request_app/urls.py
@@ -1,7 +1,8 @@
-from django.conf.urls import url
-
+from django.conf.urls import patterns, url, include
+from django.conf import settings
 from . import views
 
 urlpatterns = [
     url(r'^$',views.index,name='index'),
     ]
+    
