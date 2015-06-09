eefcc6565ff5a3d860ae982468b4ace2ba5d70c0
Adam LeBlanc <leblan7@gmail.com>
2015-06-08 22:34:40 -0700

    define models and initialized them

--- site_request_app/models.py
+++ site_request_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Request(models.Model):
+    title = models.CharField(max_length=20)
+    create_date = models.DateTimeField('date created')
+    description = models.CharField(max_length=2000)
+    votes = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.title
