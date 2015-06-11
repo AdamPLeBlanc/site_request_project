ce488c3b8dac685b1480e733a30d3cbafae9a302
Adam LeBlanc <leblan7@gmail.com>
2015-06-10 23:40:41 -0700

    reverted back to non-whitenoise

--- site_request_project/wsgi.py
+++ site_request_project/wsgi.py
@@ -1,5 +1,17 @@
-from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+"""
+WSGI config for site_request_project project.
 
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "site_request_project.settings")
+
+application = Cling(get_wsgi_application())
