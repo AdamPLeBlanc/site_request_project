62c1254718063a777b3017d11890b71fc9c5d018
Adam LeBlanc <leblan7@gmail.com>
2015-06-10 23:32:49 -0700

    changed static file maintainer to whitenoise

--- site_request_project/wsgi.py
+++ site_request_project/wsgi.py
@@ -1,17 +1,5 @@
-"""
-WSGI config for site_request_project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
-"""
-
-import os
-
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "site_request_project.settings")
-
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
